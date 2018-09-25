9828c5171e93848d93dd417ce808cf20d4c15596
Rulo Sanchez <luis.sanchez@kueski.com>
2018-09-25 12:02:36 -0500

    ädding setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 11:49:52 2018
+
+@author: raul sanchez
+"""
+
+from setuptools import setup
+
+setup(name='common',
+      version='0.1',
+      description='Machine Learning Utilities',
+      url='https://github.com/RaulSanchezVazquez/common',
+      author='Raul Sanchez-Vazquez',
+      author_email='https://github.com/RaulSanchezVazquez/common',
+      license='MIT',
+      packages=['common'],
+      zip_safe=False)
