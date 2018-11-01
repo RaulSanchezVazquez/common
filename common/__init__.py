aa27fcd65fd41833df00e50af2ef4e90386488f8
Rulo Sanchez <luis.sanchez@kueski.com>
2018-11-01 10:26:54 -0600

    Example parallel map

--- common/__init__.py
+++ common/__init__.py
@@ -10,10 +10,11 @@ from . import autoenc
 from . import date_utils
 from . import eval_utils
 from . import lgbm_utils
-from . import mean_ci
+from . import confidence_interval
 from . import nlp_utils
 from . import pandas_utils
 from . import parallel
 from . import processing_utils
 from . import weight_of_evidence
 from . import xgb_utils
+from . import compare_auc_delong_xu
